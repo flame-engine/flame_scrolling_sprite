cd22855c4be2d99976ec59ce32eb3630a607ca0f
Erick Zanardo <erickzanardoo@gmail.com>
2020-02-19 20:43:54 -0300

    Initial implementation

---
+++ example/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
+import 'package:flame_scrolling_sprite/flame_scrolling_sprite.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.util.fullScreen();
+  final size = await Flame.util.initialDimensions();
+  runApp(MyGame(size).widget);
+}
+
+class MyGame extends Game {
+
+  ScrollingSprite sprite;
+  final Size size;
+
+  MyGame(this.size) {
+    sprite = ScrollingSprite(
+        spritePath: 'city.png',
+        width: size.width,
+        horizontalSpeed: 100,
+    );
+  }
+
+  @override
+  void update(double dt) {
+    sprite.update(dt);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    if (sprite.loaded()) {
+      sprite.renderAt(0, size.height - 80, canvas);
+    }
+  }
+
+  @override
+  Color backgroundColor() => const Color(0xFF3f3f74);
+
+}
+
